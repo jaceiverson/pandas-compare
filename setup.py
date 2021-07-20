9d7a97b6aa9752d3dd6bea5c31d51cdc6b04f2df
jaceiverson <iverson.jace@gmail.com>
2021-07-20 16:24:05 -0400

    updated for v2

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="pdcompare",
-    version="0.1.0",
+    version="0.1.1",
     author="Jace Iverson",
     author_email="iverson.jace@gmail.com",
-    description="Used to compare 2 pandas DataFrame objects",
+    description="Used to compare 2 Pandas DFs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/jaceiverson/pandas-compare",
+    project_urls={
+        "Bug Tracker": "https://github.com/jaceiverson/pandas-compare",
+    },
     packages = ['pdcompare'
             ]
 
