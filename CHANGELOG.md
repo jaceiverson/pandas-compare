521f65a65d5dd0d3638da46e9ef392ec2cb3f77a
jaceiverson <iverson.jace@gmail.com>
2021-10-11 10:19:54 -0400

    updated for 0.1.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### 0.1.4 (2021-10-11)
+ - Added pct_change column for the comparison values condition in .compare()
+
 ### 0.1.3 (2021-07-20)
  - Changed import structure to allow for simpler imports
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pdcompare",
-    version="0.1.3",
+    version="0.1.4",
     author="Jace Iverson",
     author_email="iverson.jace@gmail.com",
     description="Used to compare 2 Pandas DFs",
